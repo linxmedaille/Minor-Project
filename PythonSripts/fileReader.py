521292bda75227b2969194656f7b0b28fe23c22f
linxmedaille <linxmedaille@gmail.com>
2026-02-12 15:07:21 +0000

    Test Files for debugging and python script changes

--- PythonSripts/fileReader.py
+++ PythonSripts/fileReader.py
@@ -6,10 +6,15 @@ from Bio import SeqIO
 
 # Get folder where script is
 base_dir = os.path.dirname(os.path.abspath(__file__))
+FAFile = "Mycobacterium_tuberculosis_gca_001196295.6505_4_11_.dna.toplevel.fa"
+GffFile = "Mycobacterium_tuberculosis_gca_001196295.6505_4_11.62.gff3"
 
+FATestFile = "test1.fa"
+GffTestFile = "test1.gff3"
 # Construct full paths
-currentFaFile = os.path.join(base_dir, "DataFiles", "FAFiles", "Mycobacterium_tuberculosis_gca_001196295.6505_4_11_.dna.toplevel.fa")
-currentGffFile = os.path.join(base_dir, "DataFiles", "GFF3Files", "Mycobacterium_tuberculosis_gca_001196295.6505_4_11_.dna.toplevel.fa")
+currentFaFile = os.path.join(base_dir, "DataFiles", "FAFiles", FATestFile )
+currentGffFile = os.path.join(base_dir, "DataFiles", "GFF3Files", GffTestFile)
+
 
 # Load genome
 genome = {rec.id: rec.seq for rec in SeqIO.parse(currentFaFile, "fasta")}
@@ -20,6 +25,9 @@ stop_count = 0
 
 
 with open(currentGffFile) as gff:
+    TAACount = 0
+    TAGCount = 0
+    TGACount = 0
     gff.seek(0)
     for line in gff:
         if line.startswith("#"):
@@ -29,9 +37,12 @@ with open(currentGffFile) as gff:
         parts = line.strip().split("\t")
         if len(parts) < 9:
             continue
+        
         seq_id, source, feature_type, start, end, score, strand, phase, attributes = parts
+        
         if feature_type != "CDS":
             continue
+        
         start = int(start) - 1 + int(phase)  # adjust for phase
         end = int(end)
         seq = genome[seq_id][start:end]
@@ -40,6 +51,13 @@ with open(currentGffFile) as gff:
         for i in range(0, len(seq) - 2, 3):
             codon = str(seq[i:i+3]).upper()
             if codon in stop_codons:
-                stop_count += 1
+                match codon:
+                    case "TAA":
+                        TAACount += 1
+                    case "TGA":
+                        TGACount += 1
+                    case "TAG":
+                        TAGCount += 1
+                        
 
-print(f"Total stop codons in CDS: {stop_count}")
+print(f"TAA: {TAACount} TGA: {TGACount} TAG: {TAGCount}")
